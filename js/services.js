56454137b5a422f0352c8b721fd8ee969f097a55
andriistetsiv <steciv14@gmail.com>
2022-08-27 16:56:52 +0000

    Add code

--- js/services.js
+++ js/services.js
@@ -1,6 +1,23 @@
-(function(){
+
+const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+
+const tabsItem = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(function(item) {
+   item.addEventListener("click", function(){
+   let currentBtn = item;
+   let tabId = currentBtn.getAttribute('data-tab');
+   let currentTab = document.querySelector(tabId);
+
+   
+   tabsBtn.forEach(function(item) {
+    item.classList.remove('active');
+   })
+
+   currentBtn.classList.add('active');
+   currentTab.classList.add('active');
 
 
-    
 
-})()
+   });
+});
